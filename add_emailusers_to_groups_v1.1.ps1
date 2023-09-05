387c12beb28c1f16d716ba2fbb6278290ee54b73
upioneer <hgrand@outlook.com>
2023-09-05 16:45:22 -0500

    Add files via upload

---
+++ add_emailusers_to_groups_v1.1.ps1
@@ -0,0 +1,32 @@
+﻿<#
+
+    Add array of users to array of groups
+    
+    USAGE
+        Add users to c:\temp\users.txt
+        Add groups to c:\temp\groups.txt
+        Run from elevated prompt
+        Run from account that has access to modify users
+
+#>
+
+$users = $null
+$users = Get-Content -Path 'c:\temp\users.txt'
+$groups = $null
+$groups = Get-Content -Path 'c:\temp\groups.txt'
+
+foreach ($group in $groups) {
+    foreach ($user in $users) {
+        $upn = Get-ADUser -Filter 'UserPrincipalName -eq "$user"'
+        $user = $user.Name
+        Write-Host "Adding $user to $group" -ForegroundColor Cyan
+        
+        try {
+            #Add-QADGroupMember -Identity $groups -Member $user
+            Add-ADGroupMember -Identity $group -Members $user -Verbose
+        }
+        catch {
+            $Error[0]
+        }
+    }
+}
---
+++ get_users_in_groups.ps1
@@ -0,0 +1,9 @@
+﻿$groups = @(
+'' # add groups here
+)
+
+foreach ($group in $groups) {
+    Write-Host $group -ForegroundColor Cyan
+    (Get-ADGroupMember -Identity $group).name
+    Write-Host
+}
